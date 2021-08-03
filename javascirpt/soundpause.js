603a63c12c110604111d91711c914f88bf7d1630
BerkZorbey <63355948+BerkZorbey@users.noreply.github.com>
2021-08-04 00:14:30 +0300

    Add files via upload

---
+++ javascirpt/soundpause.js
@@ -0,0 +1,70 @@
+function pause(){
+    var audio =document.getElementById("firstline") ;           
+    audio.pause();           
+    document.getElementById("firstline").muted=true;                    
+}
+function pause1(){
+     var audio1=document.getElementById("secondline") ; 
+     audio1.pause();
+     document.getElementById("secondline").muted=true;
+}
+function pause2(){
+    var audio2=document.getElementById("thirdline") ; 
+    audio2.pause();
+    document.getElementById("thirdline").muted=true;
+}
+function pause3(){
+    var audio3=document.getElementById("fourthline") ; 
+    audio3.pause();
+    document.getElementById("fourthline").muted=true;
+}
+function pause4(){
+    var audio4=document.getElementById("fifthline") ; 
+    audio4.pause();
+    document.getElementById("fifthline").muted=true;
+}
+function pause5(){
+    var audio5=document.getElementById("sixthline") ; 
+    audio5.pause();
+    document.getElementById("sixthline").muted=true;
+}
+function pause6(){
+    var audio6=document.getElementById("seventhline") ; 
+    audio6.pause();
+    document.getElementById("seventhline").muted=true;
+}
+function pause7(){
+    var audio7=document.getElementById("eighthline") ; 
+    audio7.pause();
+    document.getElementById("eighthline").muted=true;
+}
+function pause8(){
+    var audio8=document.getElementById("ninthline") ; 
+    audio8.pause();
+    document.getElementById("ninthline").muted=true;
+}
+function pause9(){
+    var audio9=document.getElementById("tenthline") ; 
+    audio9.pause();
+    document.getElementById("tenthline").muted=true;
+}
+function pause10(){
+    var audio10=document.getElementById("eleventhline") ; 
+    audio10.pause();
+    document.getElementById("eleventhline").muted=true;
+}
+function pause11(){
+    var audio11=document.getElementById("twelfthline") ; 
+    audio11.pause();
+    document.getElementById("twelfthline").muted=true;
+}
+function pause12(){
+    var audio12=document.getElementById("thirteenthline") ; 
+    audio12.pause();
+    document.getElementById("thirteenthline").muted=true;
+}
+function pause13(){
+    var audio13=document.getElementById("fourteenthline") ; 
+    audio13.pause();
+    document.getElementById("fourteenthline").muted=true;
+}
