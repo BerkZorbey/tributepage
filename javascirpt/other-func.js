0cb2ca9e7fd2fdc8be9536f8bc6730a585d2bdd4
BerkZorbey <63355948+BerkZorbey@users.noreply.github.com>
2021-08-04 00:22:48 +0300

    Add files via upload

---
+++ javascirpt/other-func.js
@@ -0,0 +1,25 @@
+//Change page En->TR
+function changepage() {
+
+    var url = "index.html";
+    if (range = 1) {
+        window.open(url, '_self');
+    }
+
+}
+//Change page TR->EN
+function changepage1() {
+
+    var url = "tributepage-en.html";
+    if (range = 1) {
+        window.open(url, '_self');
+    }
+
+}
+function hiddennote(){
+    var note=document.getElementById("note");
+    note.hidden=true;
+   if(note.hidden=true){
+        note1.hidden=true;
+    }
+}
---
+++ javascirpt/soundplay.js
@@ -0,0 +1,71 @@
+
+function play() {
+    var audio =document.getElementById("firstline");            
+    audio.play();          
+    document.getElementById("firstline").muted=false;          
+}
+function play1(){
+    var audio1=document.getElementById("secondline") ;
+     audio1.play();
+      document.getElementById("secondline").muted=false;
+}
+function play2(){
+    var audio2=document.getElementById("thirdline") ;
+     audio2.play();
+      document.getElementById("thirdline").muted=false;
+}
+function play3(){
+    var audio3=document.getElementById("fourthline") ;
+     audio3.play();
+      document.getElementById("fourthline").muted=false;
+}
+function play4(){
+    var audio4=document.getElementById("fifthline") ;
+     audio4.play();
+      document.getElementById("fifthline").muted=false;
+}
+function play5(){
+    var audio5=document.getElementById("sixthline") ;
+     audio5.play();
+      document.getElementById("sixthline").muted=false;
+}
+function play6(){
+    var audio6=document.getElementById("seventhline") ;
+     audio6.play();
+      document.getElementById("seventhline").muted=false;
+}
+function play7(){
+    var audio7=document.getElementById("eighthline") ;
+     audio7.play();
+      document.getElementById("eighthline").muted=false;
+}
+function play8(){
+    var audio8=document.getElementById("ninthline") ;
+     audio8.play();
+      document.getElementById("ninthline").muted=false;
+}
+function play9(){
+    var audio9=document.getElementById("tenthline") ;
+     audio9.play();
+      document.getElementById("tenthline").muted=false;
+}
+function play10(){
+    var audio10=document.getElementById("eleventhline") ;
+     audio10.play();
+      document.getElementById("eleventhline").muted=false;
+}
+function play11(){
+    var audio11=document.getElementById("twelfthline") ;
+     audio11.play();
+      document.getElementById("twelfthline").muted=false;
+}
+function play12(){
+    var audio12=document.getElementById("thirteenthline") ;
+     audio12.play();
+      document.getElementById("thirteenthline").muted=false;
+}
+function play13(){
+    var audio13=document.getElementById("fourteenthline") ;
+     audio13.play();
+      document.getElementById("fourteenthline").muted=false;
+}
